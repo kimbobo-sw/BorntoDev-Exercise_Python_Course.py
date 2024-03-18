7d5e158c22c4464d7c5dd874f094f2b003536a32
Kittisak Limpongsa <62006117+kimbobo-sw@users.noreply.github.com>
2024-03-18 13:49:27 +0700

    UpDate Excercise 4

---
+++ Exercise_4_Kittisak_Limpongsa.py
@@ -0,0 +1,19 @@
+''''''''''
+x = str(input())  # รับข้อความที่หนึ่งจากผู้ใช้
+y = str(input())    # รับข้อความที่สองจากผู้ใช้
+z = str(input())    # รับข้อความที่สามจากผู้ใช้
+
+# ต่อสตริงเข้าด้วยกัน โดยเพิ่มเว้นวรรคระหว่างข้อความ x และ z ด้วยวิธีการ + " "
+result = x + " " + z + " " + y
+print( result)
+'''''''''''
+
+a = int(input(" Input Your  Foundation English Score:"))
+b = int(input("Input Your General Business Score:"))
+c = int(input("Input Your Introduction to Computer Systems:"))
+d = int(input("Input Your Computer Programming Score:"))
+
+print(" You  Foundation English Score:",a)
+print(" Your General Business Score:",b)
+print("Your Introduction to Computer Systems:",c)
+print("Your Computer Programming Score:",d)
