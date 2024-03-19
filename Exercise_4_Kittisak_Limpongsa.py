c9b9d544b33fb1978c902f917fede048c18a5ca4
Kittisak Limpongsa <62006117+kimbobo-sw@users.noreply.github.com>
2024-03-20 06:51:14 +0700

    Exercise 5

--- Exercise_4_Kittisak_Limpongsa.py
+++ Exercise_4_Kittisak_Limpongsa.py
@@ -8,12 +8,6 @@ result = x + " " + z + " " + y
 print( result)
 '''''''''''
 
-a = int(input(" Input Your  Foundation English Score:"))
-b = int(input("Input Your General Business Score:"))
-c = int(input("Input Your Introduction to Computer Systems:"))
-d = int(input("Input Your Computer Programming Score:"))
+Username ="20"
 
-print(" You  Foundation English Score:",a)
-print(" Your General Business Score:",b)
-print("Your Introduction to Computer Systems:",c)
-print("Your Computer Programming Score:",d)
+print (Username + 10)
---
+++ Exercise_5_Kittisak_Limpongsa.py
@@ -0,0 +1,22 @@
+#10 + 2 = 12
+#10 - 2 = 8
+#10 * 2 = 20
+#10 / 2 = 5
+
+
+""""""""" #วิธีทำเเบบที่ 1
+x = int(input("input your values:  "))
+y = 2
+z = x+y,x-y,x*y,x/y
+
+print(z)
+"""""""""""
+
+# วิธีทำเเบบที่ 2
+x = int(input("Input yout values: "))
+y = 2
+
+print(x,"+",y,"=",x+y)
+print(x,"+",y,"=",x-y)
+print(x,"+",y,"=",x*y)
+print(x,"+",y,"=",x/y)
