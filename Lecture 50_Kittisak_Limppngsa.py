b4ca810dcd6a185b195b190d10ace99aba01c2f4
Kittisak Limpongsa <62006117+kimbobo-sw@users.noreply.github.com>
2024-03-29 02:18:13 +0700

    Lecture 50

---
+++ Lecture 50_Kittisak_Limppngsa.py
@@ -0,0 +1,24 @@
+x = int(input())
+y = int(input())
+def sum (x,y):
+    print(x+y)
+
+sum(x,y)
+
+
+def sum (x,y):
+    print(x-y)
+
+sum(x,y)
+
+def sum (x,y):
+    print(x*y)
+
+sum(x,y)
+
+
+def sum (x,y):
+    c = int(x/y)
+    print(c)
+
+sum(x,y)
